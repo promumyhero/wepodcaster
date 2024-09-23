c07f94e77f1b7cf1d245a72467217b647fa6f163
Ridho Hery Winarto <2009041@uwhs.ac.id>
2024-09-23 14:09:50 +0700

    add routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
     <div className="mt-9 flex-col gap-9">
       <section className="flex flex-col gap-5">
-        <h1>
-
+        <h1 className=" text-20 font-bold text-white-1">
+          Trending Podcast
         </h1>
       </section>
     </div>
