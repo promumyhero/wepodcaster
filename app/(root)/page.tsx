1d2cfeaa26249666a75cd93759022ac0e4a6e1da
Ridho Hery Winarto <2009041@uwhs.ac.id>
2024-09-25 15:11:26 +0700

    added create podcast & generate podcast

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ import { api } from "@/convex/_generated/api";
 
 const Home = () => {
   // demo data
-  const tasks = useQuery(api.tasks.get);
+  //const tasks = useQuery(api.tasks.get);
 
   return (
     <div className="mt-9 flex-col gap-9">
@@ -19,7 +19,7 @@ const Home = () => {
         </h1>
         
         <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
-          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+          {/*tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)*/}
         </div>
 
         <div className="podcast_grid">
